8a2a69769d2a841b0b1217b527a346c3d9de2f60
Osita Precious <foroseetarh@gmail.com>
2025-08-31 10:00:35 +0100

    cart functionality with fastapi

--- shop.py
+++ shop.py
@@ -1,1 +1,29 @@
-from
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+class Cart(BaseModel):
+    user_id: int
+    product_id: int
+    quantity: int
+
+cart_db = []
+
+@app.get("/")
+def home():
+    return{"message": "Welcome to the Cart API!"}
+
+@app.get("/carts/{user_id}")
+def check_out(user_id: int):
+    user_cart = [cart for cart in cart_db if cart["user_id"] == user_id]
+    return user_cart
+
+@app.post("/carts")
+def create_cart(cart: Cart):
+    cart_data = {
+        "user_id": cart.user_id,
+        "product_id": cart.product_id,
+        "quantity": cart.quantity
+        }
+    cart_db.append(cart_data)
+    return{"Cart": cart_db}
